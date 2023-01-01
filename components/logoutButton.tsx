b1c5cfd90b5cd8a395a82f69c2e0bf232d3519c2
kasutu <jeromecabugwason2@protonmail.com>
2023-01-01 20:42:39 +0800

    jwt: Refactor
    Description:
    rollback jwt to default behavior
    
    Expected behavior:
    breaking change with `auth` classes
    
    Co-authored-by: Yui187 <Yui187@users.noreply.github.com>

--- components/logoutButton.tsx
+++ components/logoutButton.tsx
@@ -1,25 +1,17 @@
 import Axios from 'axios';
 import { useRouter } from 'next/router';
+import { logoutInteractor } from '../lib/auth/logoutInteractor';
 
 export function LogoutButton() {
   const router = useRouter();
 
-  const logout = (event:any) => {
+  const logout = (event: any) => {
     event.preventDefault();
 
-    Axios.get('http://localhost:8080/auth/logout')
-    .then((res) => {
-      if (res.status === 200) {
-        // Redirect to the login page
-        router.push('/');
-        }})
-    .catch((error) => {
-      console.error(error);
-    });
+    logoutInteractor();
+
+    router.push('/');
   };
 
-  return (
-    <button onClick={logout}>Logout</button>
-  );
+  return <button onClick={logout}>Logout</button>;
 }
-
--- lib/auth/registerInteractor.ts
+++ lib/auth/registerInteractor.ts
@@ -1,14 +1,28 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { UserRequest } from '../user/userRequest';
 
 export async function registerInteractor(
   userRequest: UserRequest
 ): Promise<boolean> {
-  const url = 'http://localhost:8080/auth/register';
+  const url = 'http://localhost:8080/register';
 
   try {
-    await axios.post(url, userRequest);
-  } catch (error) {
+    let res = await axios.post(url, userRequest);
+    console.log(res.data);
+  } catch (err: any) {
+    if (err.response) {
+      // The client was given an error response (5xx, 4xx)
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+    } else if (err.request) {
+      // The client never received a response, and the request was never left
+      console.log(err.request);
+    } else {
+      // Anything else
+      console.log('Error', err.message);
+    }
+
     return false;
   }
 
