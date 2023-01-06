c83b656aeb9fff77f1cc97527c0ec5f7f8cfd2aa
kasutu <jeromecabugwason2@protonmail.com>
2023-01-06 10:10:37 +0800

    dep[loy

--- lib/apiInteractor/apiInstance.ts
+++ lib/apiInteractor/apiInstance.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+// export const axiosInstance = axios.create({
+//   baseURL: 'http://34.143.216.186:28762/api',
+// });
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://34.143.216.186:28762/api',
+  baseURL: 'http://api:28762/api',
 });
