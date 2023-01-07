3f24d960cf3f3103b0fccf850bdc2490b22eed7c
kasutu <jeromecabugwason2@protonmail.com>
2023-01-07 20:12:53 +0800

    patch cant copy files

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+    output: 'standalone',
   },
 };
 
