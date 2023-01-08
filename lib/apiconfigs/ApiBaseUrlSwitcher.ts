237fd48be507a95785ad672cd6ee9a7c1867788e
kasutu <jeromecabugwason2@protonmail.com>
2023-01-08 16:23:28 +0800

    updated base url

--- lib/apiconfigs/ApiBaseUrlSwitcher.ts
+++ lib/apiconfigs/ApiBaseUrlSwitcher.ts
@@ -1,11 +1,12 @@
+const devURL = 'http://localhost:20000/api';
+const productionURL = 'http://172.19.0.1:20001/api';
+
 export const ApiBaseUrlSwitcher = () => {
   if (process.env.NODE_ENV !== 'production') {
-    console.debug('Using dev API url');
-
-    return 'http://localhost:8081/api';
-  } else {
-    console.debug('Using production API url');
-
-    return 'http://api:28762/api';
+    console.debug('Using dev API url: ', devURL);
+    return devURL;
   }
+
+  console.debug('Using production API url: ', productionURL);
+  return productionURL;
 };
