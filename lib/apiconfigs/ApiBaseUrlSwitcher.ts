8a6ffb4fb58b4fcd1ad545c4e148dddc97132783
kasutu <jeromecabugwason2@protonmail.com>
2023-01-08 16:38:31 +0800

    updated to docker network url

--- lib/apiconfigs/ApiBaseUrlSwitcher.ts
+++ lib/apiconfigs/ApiBaseUrlSwitcher.ts
@@ -1,5 +1,5 @@
 const devURL = 'http://localhost:20000/api';
-const productionURL = 'http://34.143.216.186:20001/api';
+const productionURL = 'http://172.19.0.1:20001/api';
 
 export const ApiBaseUrlSwitcher = () => {
   if (process.env.NODE_ENV !== 'production') {
