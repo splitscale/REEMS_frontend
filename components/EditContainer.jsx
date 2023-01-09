26186f8f80497e8bb990a7a56083a54b465ab7ea
Yoshifumi12 <yoshifumisuzuki81194@gmail.com>
2023-01-09 17:41:02 +0800

    delete and edit container
    idk if it works

--- components/EditContainer.jsx
+++ components/EditContainer.jsx
@@ -1,13 +1,45 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { any } from "react-native/Libraries/Text/TextNativeComponent";
 
 
 function EditContainer({container}) {
     const [show, setShow] = useState(false);
+    const router = useRouter();
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const [name, setName] = useState('');
+    
+    const config = {
+      method: 'PUT',
+      headers: {
+        authorization: getAuthCookie(),
+      },
+      data: {
+        uid: getUidCookie(),
+        name: name,
+      },
+    }
+
+    console.log(config)
+
+    const editContainer = async (event) => {
+
+      event.preventDefault();
+
+      await fetch(`/containers/${container.Id}`, config)
+      .then(() => {
+        alert("Edit Success")
+        router.push('/');
+        handleClose();
+      })
+      .catch(error => {
+        console.log('Error', error.message);
+        alert("Edit Failed");
+      })
+  }
     return(
     <>
     <Button variant="primary" onClick={handleShow}>
@@ -25,8 +57,8 @@ function EditContainer({container}) {
         <input
           type="text"
           name="title"
-        //   value={container.title}
-          onChange={(e) => handleChange(e)}
+          value={name}
+          onChange={(e) => setName(e)}
           className="h-10 w-96 border mt-2 px-2 py-2"
         ></input>
       </Modal.Body>
@@ -34,9 +66,9 @@ function EditContainer({container}) {
         <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
-        {/* <Button variant="primary" onClick={saveContainer}>
+         <Button variant="primary" onClick={editContainer}>
           Save Changes
-        </Button> */}
+        </Button> 
       </Modal.Footer>
     </Modal>
   </>
--- components/deleteContainer.jsx
+++ components/deleteContainer.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-
-export function DeleteContainer({container}) {
-  const handleDelete = (id) => {
+async function DeleteContainer(container){
     if (window.confirm('Are you sure you want to delete this container?')) {
-      axios.delete(`/api/containers/${container.id}`)
-        .then(response => {
-          setSuccess(response.data.message)
-        })
-        .catch(error => {
-          setError(error.response.data.message)
-        })
+      await fetch ( `/containers/${container.Id}`,{method: 'DELETE'})
+      .then(response => {
+        setSuccess(response.data.message)
+      })
+      .catch(error => {
+        setError(error.response.data.message)
+      })
     }
   }
-}
+
+  export default DeleteContainer;
+
+
+
+
 
