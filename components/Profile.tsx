08a6a004dc8bd27e671dcd081a8384ede1fab964
Steve <steven.ballaret-17@cpu.edu.ph>
2023-05-28 18:14:23 +0800

    changed profile

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 
 export default function Profile() {
   const [imageSrc, setImageSrc] = useState("user-icon.png");
-  const [username]= useState("admin")
-  const [password]= useState("admin")
 
   function handleImageChange(event: React.ChangeEvent<HTMLInputElement>) {
     const reader = new FileReader();
     reader.readAsDataURL(event.target.files![0]);
-    reader.onload = () => setImageSrc(reader.result as string);
+    reader.onload = () => {
+      setImageSrc(reader.result as string);
+    };
   }
 
   return (
@@ -16,22 +16,29 @@ export default function Profile() {
       <div className="flex flex-col items-center mt-20">
         <label htmlFor="file-input">
           <div className="w-60 h-60 rounded-full overflow-hidden">
-            <img src={imageSrc} alt="Your Profile Picture" className="w-full h-full object-cover" />
+            <img
+              src={imageSrc}
+              alt="Your Profile Picture"
+              className="w-full h-full object-cover"
+            />
           </div>
-
         </label>
-        <input id="file-input" type="file" accept="image/*" onChange={handleImageChange} className="sr-only" />
+        <input
+          id="file-input"
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          className="sr-only"
+        />
 
-        <div className="text-2xl mb-20 mt-1">
-          Profile Photo
-        </div>
+        <div className="text-2xl mb-20 mt-1">Profile Photo</div>
       </div>
 
       <div className="card mb-8">
         <div className="card-body bg-white">
           <div className="mt-4 my-8 mx-40 rounded-lg">
-            <p className="text-xl">Username: {username}</p>
-            <p className="text-xl">Password: {password} </p>
+            <p className="text-xl">Username: </p>
+            <p className="text-xl">Email: </p>
           </div>
         </div>
       </div>
